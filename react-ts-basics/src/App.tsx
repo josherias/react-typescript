5b5595484ff116ce1c97fd76161abbbce158b92a
Lubuulwa Elias <elubuulwa@renu.ac.ug>
2024-03-22 08:44:32 +0300

    Started working with react and typescript

---
+++ react-ts-basics/src/App.tsx
@@ -0,0 +1,11 @@
+import CourseGoal from "./components/CourseGoal";
+
+export default function App() {
+  return (
+    <main>
+      <CourseGoal title="Learn react + TS">
+        <p>Learn it From ground up</p>
+      </CourseGoal>
+    </main>
+  );
+}
